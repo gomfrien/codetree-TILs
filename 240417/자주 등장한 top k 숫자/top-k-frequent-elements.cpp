a98ca8069b80f187b300a48fc22c7116ec2237a3
gomfrien <99187741+gomfrien@users.noreply.github.com>
2024-04-17 02:08:54 +0900

    Title: 자주 등장한 top k 숫자, Time: 14ms, Memory: 3MB, Status: Wrong Answer - CodeTree

--- 240417/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
+++ 240417/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
@@ -4,8 +4,49 @@
 
 using namespace std;
 
-unordered_map <int,int> buf;
+unordered_map <int,int> keymap;
 vector <int> Cnt[110000];
+
+struct Node 
+{
+    int num;
+
+    Node* pre;
+    Node* next;
+
+}*head[110000], *tail[110000], buf[1100000];
+
+int bCnt = 0;
+
+void init()
+{
+    for(int i = 0 ; i < 110000; i++)
+    {
+        buf[bCnt] = {0,nullptr,nullptr};
+        head[i] = &buf[bCnt++];
+
+        buf[bCnt] = {110000,nullptr,nullptr};
+        tail[i] = &buf[bCnt++];
+
+        head[i]->next = tail[i];
+        tail[i]->pre = head[i];
+    }
+}
+
+void push_back(int key, int num)
+{
+    buf[bCnt] = {num,nullptr,nullptr};
+    Node* p = &buf[bCnt++];
+    for(Node* c = head[key]->next ; c=c->next; c->next != nullptr)
+    {
+        if(p->num < c->num)
+        {
+            p->pre = c->pre;
+            p->next = c;
+            c->pre = p;
+        }
+    }
+}
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -19,10 +60,10 @@ int main() {
         int t = 0; 
         cin>>t;
 
-        buf[t]++;
+        keymap[t]++;
     }
 
-    for(auto &t : buf)
+    for(auto &t : keymap)
     {
             int key = t.second;
             Cnt[key].push_back(t.first);
